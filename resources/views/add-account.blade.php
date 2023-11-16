69b572ed2fa41cac5655878ec2775d58e8441c2b
stestellouch <stella.maestre@etu.univ-smb.fr>
2023-11-16 08:30:23 +0000

    add-account modification js

--- resources/views/add-account.blade.php
+++ resources/views/add-account.blade.php
@@ -4,7 +4,7 @@
 
 
 
-<form method="post" id="formulaire">
+<form method="post" class="formulaire">
 <div id="account">
 <div class="form-login">
 <h1>Créez un compte</h1>
@@ -18,7 +18,7 @@
 <input type="radio" id="entreprise" name="typeAccount" value="entreprise" />
 <label for="entreprise">Pour votre entreprise</label>
 </div><br>
-<p>Vous avez deja un compte ?<a href="">Me connecter</a></p> 
+<p>Vous avez deja un compte ?<a class="login-link">Me connecter</a></p> 
 
 <div class="separation"></div>
 
@@ -33,22 +33,33 @@
 <h1>Bonjour !</h1>
 <p>Connectez-vous pour découvrir toutes nos fonctionnalités.</p>
 <div>
-<input type="email" id="email"/>
 <label for="email">Email</label>
+<input type="email" id="email"/>
+
 </div>
 
 <div>
-<input type="password" id="password" />
 <label for="password">Mot de Passe</label>
+<input type="password" id="password" />
+
 </div><br>
-<p><a href="">Mot de passe oublié</a></p> 
+<p><a class="register-link">Mot de passe oublié</a></p> 
 
 <input type="button" value=" Se connecter ">
 
-<p class="petit">À défaut, en application de l’article L 132-2 du Code de la consommation qui sanctionne les pratiques commerciales trompeuses, vous encourez une peine d’emprisonnement de 2 ans et une amende de 300 000 euros.</p>
 </div>
 
 </div>
 
 @csrf
 </form>
+
+
+<script>
+
+    const formulaire = document.querySelector('.formulaire')
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+
+
+</script>
