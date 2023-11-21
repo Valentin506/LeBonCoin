29d3154f2d71028cca725ad5a0d410ae69a0e2ac
stestellouch <stella.maestre@etu.univ-smb.fr>
2023-11-21 09:32:50 +0000

    add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="app.css">
     <title>leboncoin, site de peties annonces gratuites</title>
+    <link rel="icon" type="image/x-icon" href="https://img.utdstc.com/icon/f01/c30/f01c30e1ae7730771565e60e5b26d888a6a6a49f2112235ba23d30abbc4b4923:200">   
 </head>
 <body>
     
