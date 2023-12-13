61385a9c6b67ea53e1f69e7c10ce505dc86e52c6
stestellouch <stella.maestre@etu.univ-smb.fr>
2023-12-13 10:46:33 +0000

    View reservation

--- resources/views/my-bookings.blade.php
+++ resources/views/my-bookings.blade.php
@@ -12,8 +12,11 @@
 @foreach($bookings as $booking)
         <div class="booking-details">
             <p>Réservation ID: {{ $booking->idfiche }}</p>
-            <p>Adultes: {{ $booking->nbadultes}}</p>
-            <p>Date de fin: {{ $booking->date_fin }}</p>
+            <p>Adultes: {{ $booking->nbadulte}}</p>
+            <p>Enfants: {{ $booking->nbenfant}}</p>
+            <p>Bébés: {{ $booking->nbbebe}}</p>
+            <p>Animaux: {{ $booking->nbanimaux}}</p>
+            
             <!-- Ajoutez d'autres détails selon votre structure de données -->
         </div>
     @endforeach
